dadea2478600f162983cc21804de221439cdfa3e
badrbelabrik1 <badr.belabrik@gmail.com>
2025-11-02 23:45:24 +0100

    miniprojet time simulation part

--- miniprojects/miniprojet1.js
+++ miniprojects/miniprojet1.js
@@ -3,20 +3,21 @@ const prompt = require("prompt-sync")();
 const taxis = [
 { id: 1, position: 5, available: true, timeRemaining: 0, totalRides:
 0 },
-{ id: 2, position: 12, available: false, timeRemaining: 0,
+{ id: 2, position: 12, available: true, timeRemaining: 0,
 totalRides: 0 },
-{ id: 3, position: 20, available: false, timeRemaining: 0,
+{ id: 3, position: 20, available: true, timeRemaining: 0,
 totalRides: 0 }
 ]
 ;
 
 const requests = [
-{ reqId: 1, position: 10, duration: 3, time: 0 },
-{ reqId: 2, position: 3, duration: 4, time: 1 },
-{ reqId: 3, position: 18, duration: 2, time: 2 },
-{ reqId: 4, position: 7, duration: 5, time: 3 }
+{ reqId: 1, position: 10, duration: 3, time: 0, destination: 13},
+{ reqId: 2, position: 3, duration: 4, time: 1, destination: 19 },
+{ reqId: 3, position: 18, duration: 2, time: 2, destination: 20 },
+{ reqId: 4, position: 7, duration: 5, time: 3, destination: 4 }
 ];
 
+const completedreq = [];
 const waitingqueue = [];
 
 function createreq(){
@@ -36,6 +37,7 @@ function createreq(){
 }
 
 
+
 function findtaxi(request){
     // checking for available taxi
     let mindistance = Infinity;
@@ -49,45 +51,61 @@ function findtaxi(request){
                     closetaxi = taxis[i];
                 }
             }
-        } 
-        if(!closetaxi){
-            console.log("There is no available taxi, your request will be in waiting queue")
-            waitingqueue.push(request);
-            return null;
-            }
-    console.log(`the request ${request.reqId} has been assigned to taxi ${closetaxi.id}`)
+             } 
 
-    closetaxi.available = false;
-    closetaxi.timeRemaining = request.duration;
-    closetaxi.destination = request.destination;
-    closetaxi.totalRides++;
-    return mindistance;
+            if(closetaxi){
+            closetaxi.available = false;
+            closetaxi.timeRemaining = request.duration;
+            closetaxi.destination = request.destination;
+            closetaxi.currentreq = request.reqId;
+            closetaxi.reqposition = request.position;
+            closetaxi.distance = mindistance;
+            completedreq.push(request);
+            console.log(`the request ${request.reqId} has been assigned to taxi ${closetaxi.id}`)
+        }
+        else if(!closetaxi){
+            console.log(`No taxi available for request ${request.reqId}, waiting...`);
+            waitingqueue.push(request);
+            return;
+            }
+       
+        
+        
+
+    const index = requests.indexOf(request);
+    if (index > -1) requests.splice(index, 1);
+    
 }
 
 function timesimulation(){
     let timer = 0;
-    while(waitingqueue.length > 0 || taxis.some(item => item.timeRemaining > 0)){
+    while( requests.length > 0  || waitingqueue.length > 0 || taxis.some(item => item.timeRemaining > 0)){
          console.log(`Minute :${timer}`)
          
-         requests.forEach(item => {
+         for (let i = requests.length - 1; i >= 0; i--) {
+            let item = requests[i];
             if(item.time === timer){
             findtaxi(item);
             }
-            });
+            }
 
-        if(waitingqueue.length > 0 && taxis.some(item => item.available)){
-            let first = waitingqueue.shift();
-            findtaxi(first);
-        }
+        while(waitingqueue.length > 0 && taxis.some(item => item.available)){
+            const req = waitingqueue.shift();
+            findtaxi(req);
+            }
 
          taxis.forEach(item => {
          if (item.timeRemaining > 0) {
-            console.log(`-> Taxi ${item.id} assigned (distance : )`) 
+            console.log(`Request ${item.currentreq} at position ${item.reqposition} -> Taxi ${item.id} assigned  : (distance: ${item.distance})`) 
             item.timeRemaining--;
             if(item.timeRemaining == 0){
                 item.available = true;
                 item.position = item.destination;
+                item.totalRides++;
                 delete item.destination;
+                delete item.currentreq;
+                delete item.reqposition;
+                delete item.distance;
             }
             }
     });
@@ -95,5 +113,6 @@ function timesimulation(){
     }
 }
 
-
+// let request = createreq();
 timesimulation();
+
---
+++ miniprojects/test.js
@@ -0,0 +1,67 @@
+const { createServer } = require('node:http');
+const path = require('path'); // Required for path manipulation
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs'); // Set EJS as the templating engine
+// Middleware to parse URL-encoded data (from HTML forms)
+app.use(express.urlencoded({ extended: true })); 
+// Middleware to parse JSON data (if you also send JSON)
+app.use(express.json());
+// Serve static files from the 'public' directory
+//app.use(express.static(path.join(__dirname, 'public')));
+app.post('/submit-newreq', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    console.log('Received form data:');
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    res.send("Thank you for submitting, ${username}!");
+});
+app.get('/', (req, res) => {
+    //res.sendFile(path.join(__dirname, 'public', 'page.html'));
+    // res.send('Welcome to the homepage!');
+    const userData = {
+
+            name: 'John Doe',
+            email: 'john.doe@example.com'
+        };
+        res.render('page', { user: userData }); // Render 'index.ejs' and pass 'userData'
+    });
+app.get('/badro', (req, res) => {
+    console.log("ch3aaand dimass dimak");   
+    const data = {
+    message: 'Hello from Node.js!',
+    timestamp: new Date().toISOString()
+  };
+  res.json(data); // Send JSON response   
+    });
+
+app.listen(port, () => {
+        console.log("Server listening at http://localhost:${port}");
+    });
+
+
+
+
+
+    function createreq(){
+    const position = Number(prompt("Enter your position :"))
+    const destination = Number(prompt("Enter your destination :"))
+    const duration = Number(prompt("Enter the duration :"))
+    let request = {
+        reqId: requests.length + 1,
+        position: position,
+        destination: destination,
+        duration: duration,
+        time: 0,
+    }
+    requests.push(request);
+    console.log("Request added succesfully!");
+    return request;
+}
